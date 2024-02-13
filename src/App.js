79e7b57bef9b55c7dc791ca18317305a94447744
akashgupta200134 <akashgupta_ce_2021@ltce.in>
2024-02-14 04:25:06 +0530

    uploaded full project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
-import './App.css';
+import './App.css'
+import React, { useState } from 'react';
+import data from './components/data';
 
-function App() {
-  return (
-    <div className="App">APP</div>
-  );
-}
+import Tours from './components/Tours';
+
+
+const App = () => {
+
+  const [tours, SetTours] = useState(data);
+
+
+  function removeTour(id) {
+    const newTours = tours.filter(tour => tour.id !== id);
+    SetTours(newTours);
+  }
+
+  if(tours.length === 0) {
+
+    return(
+      <div className='refresh'>
+            <h2> No Tours Left</h2>
+            <button  className='refresh-btn' onClick={() => SetTours(data)}>
+              Refresh
+            </button>
+
+      </div>
+
+
+    );
+
+  }
+
+ return( 
+ 
+ <div>
+
+  <Tours tours={tours} removeTour={removeTour}></Tours>
+
+ </div>
+
+  )
+};
 
 export default App;
--- src/components/data.js
+++ src/components/data.js
@@ -1,12 +1,107 @@
 
-const data = {
-
+const data = [
+{
     id:1,
     name: "Agra",
-    info : "Agra is a city in northen India, Known for the iconic Taj Mahal",
+    info: "Agra is a city in northen India,Known for the iconic Taj Mahal,the Taj Mahal is set high on a hill above the river.Further banks is the renowned Agra Fort, as well as a selection of other fascinating tombs and mausoleums.",
+    image: "https://i.pinimg.com/564x/a2/45/13/a245133998244908efecadf2798e5e5f.jpg",
+    price:"45,999",
+
+},
+
+{
+
+    id:2,
+    name: "Jaipur",
+    info: `Jaipur formerly known as Jeypore, The Hawa Mahal is a palace in the city of Jaipur, Rajasthan, India. Built from red and pink sandstone, it is on the edge of the City Palace, Jaipur, and extends to the Zenana, or women's chambers.`,
+    image: "https://i.pinimg.com/564x/df/62/42/df62423b3caf511486296013258c9766.jpg",
+    price:"51,999",
 
 
+},
+
+{
+
+    id:3,
+    name: "Goa",
+    info: "The state of Goa, in India, is famous for its beaches and places of worship. Tourism is its primary industry, and is generally focused on the coastal areas of Goa, with decreased tourist activity inland..",
+    image: "https://i.pinimg.com/736x/a6/74/f4/a674f407d8af1d8987e6599c0e51d5bc.jpg",
+    price:"41,999",
 
 
+},
 
-}
+
+{
+
+    id:4,
+    name: "Jaisalmer",
+    info: `The city of Jaisalmer . This 'Golden City' is located close to the Pakistan border and in close proximity to the Thar Desert. The city's most prominent landmark is the Jaisalmer Fort, also called Sonar Qila (Golden Fort)..`,
+    image: "https://i.pinimg.com/564x/3b/f4/09/3bf409014875a646a733b5394868351d.jpg",
+    price:"47,999",
+
+
+},
+
+
+{
+
+    id:5,
+    name: "Varanasi",
+    info: "Abode of Lord Shiva and Parvati, the origins of Varanasi are yet unknown. Ganges in Varanasi is believed to have the power to wash away the sins of mortals. Hindus believe that one who is graced to die on the land of Varanasi would attain salvation and freedom from the cycle of birth and re-birth.",
+    image: "https://i.pinimg.com/736x/c8/5b/92/c85b92f35284ea7a3aee471ffbd26625.jpg",
+    price:"38,999",
+
+
+},
+
+
+{
+
+    id:6,
+    name: "Kochi",
+    info: "Kochi (also known as Cochin) is a city in southwest India's coastal Kerala state. It has been a port since 1341, when a flood carved out its harbor and opened it to Arab, Chinese and European merchants. Sites reflecting those influences include Fort Kochi, a settlement with tiled colonial bungalows and diverse houses of worship.",
+    image: "https://i.pinimg.com/564x/54/ca/33/54ca33145d369d30f57e9e07d4908ee7.jpg",
+    price:"39,999",
+
+
+},
+
+
+{
+
+    id:7,
+    name: "Ayodhya",
+    info: `Ayodhya is a city situated on the banks of the Sarayu river in the Indian state of Uttar Pradesh. It is the administrative headquarters of the Ayodhya district as well as the Ayodhya division of Uttar Pradesh, India. Ayodhya was historically known as Saketa.`,
+    image: "https://i.pinimg.com/564x/a4/a0/e1/a4a0e1cc87145b45581de24d4fb7868b.jpg",
+    price:"30,999",
+
+
+},
+
+
+{
+
+    id:8,
+    name: "Kerala",
+    info: "Kerala, a state on India's tropical Malabar Coast, has nearly 600km of Arabian Sea shoreline. It's known for its palm-lined beaches and backwaters,a network of canals.and other sanctuaries, are home to elephants,monkeys and tigers.",
+    image: "https://i.pinimg.com/564x/26/f4/04/26f404b6484db3d45c8e895dacfe961a.jpg",
+    price:"45,999",
+
+
+},
+
+{
+
+    id:9,
+    name: "Mumbai",
+    info: `Mumbai (formerly called Bombay).On the Mumbai stands the iconic Gateway of India Offshore,nearby Elephanta Island holds ancient cave temples dedicated to the Hindu god Shiva.The city also famous as the heart of the Bollywood industry.`,
+    image: "https://i.pinimg.com/564x/ac/a5/63/aca56396218cfb0bdb86c32e317284ba.jpg",
+    price:"55,999",
+
+
+},
+
+]
+
+export default data;
